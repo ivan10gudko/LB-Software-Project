966b26c71dce655b2e4f52c1228e55e1f220bb2b
Walpys <artemkrat073@gmail.com>
2025-10-28 20:32:28 +0200

    Refactored SearchController(return Json )

--- backend/demo/src/main/java/project_z/demo/controllers/SearchController.java
+++ backend/demo/src/main/java/project_z/demo/controllers/SearchController.java
@@ -1,13 +1,10 @@
 package project_z.demo.controllers;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import project_z.demo.entity.TitleEntity;
 import project_z.demo.services.TitleSearchService;
 import project_z.demo.services.TranslationService;
 
@@ -25,9 +22,9 @@ public class SearchController {
     }
         */
     @GetMapping(path = "/Search")
-    public List<TitleEntity> Search(@RequestParam("q") String text) {
+    public String Search(@RequestParam("q") String text) {
          String translatedService = translationService.translateToEnglish(text);
-         List<TitleEntity> response = titleSearchService.searchTitle(translatedService);
+         String response = titleSearchService.searchTitle(translatedService);
          return response;
     }
     
