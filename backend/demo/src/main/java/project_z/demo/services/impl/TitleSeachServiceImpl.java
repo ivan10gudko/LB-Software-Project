f9a66fafb23670ebd306fc37f4b3e855f0c76e51
Walpys <artemkrat073@gmail.com>
2025-10-28 20:41:35 +0200

    refactored TitleServiceImpl

--- backend/demo/src/main/java/project_z/demo/services/impl/TitleSeachServiceImpl.java
+++ backend/demo/src/main/java/project_z/demo/services/impl/TitleSeachServiceImpl.java
@@ -3,8 +3,6 @@ package project_z.demo.services.impl;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,9 +15,7 @@ public class TitleSeachServiceImpl implements TitleSearchService {
    public String searchTitle(String text){
     String encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8);
     String url = "https://api.jikan.moe/v4/anime?q="+ encodedText + "&limit=5";
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    String res = restTemplate.getForObject(url, String.class);
-    return res;
+     return restTemplate.getForObject(url, String.class);
+    
 }
 }
