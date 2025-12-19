a3b6a2dfb3c33123a6dbbaf21e8b5f3c610691e1
Ivan Hudko <giv.10072007@gmail.com>
2025-12-19 18:42:40 +0200

    change search engine(artem from my laptop)

--- backend/demo/src/main/java/project_z/demo/controllers/SearchController.java
+++ backend/demo/src/main/java/project_z/demo/controllers/SearchController.java
@@ -24,6 +24,7 @@ public class SearchController {
     @GetMapping(path = "/api/v1/search")
     public String Search(@RequestParam("q") String text, @RequestParam("page") int page) {
          String translatedService = translationService.translateToEnglish(text);
+         
          String response = titleSearchService.searchTitle(translatedService, page);
          return response;
     }
